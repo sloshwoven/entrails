f5aee769f2d19cd26d207db7d61de1fb5294607c
wovenslosh <wovenslosh@gmail.com>
2013-03-10 16:07:29 -0400

    Added usage message and -h/-help/--help options for printing it.

--- bin/entrails.js
+++ bin/entrails.js
@@ -4,12 +4,16 @@ var fs       = require('fs'),
     entrails = require('entrails'),
     promFs   = require('promised-io/fs');
 
+var USAGE = 'Usage: entrails <file>';
+
 (function(args) {
     var main = function(args) {
         if (args.length < 1) {
             die('too few arguments');
         } else if (args.length > 1) {
             die('too many arguments');
+        } else if (args[0] === '-h' || args[0] === '-help' || args[0] === '--help') {
+            console.log(USAGE);
         } else {
             transformFile(args[0]);
         }
@@ -29,6 +33,7 @@ var fs       = require('fs'),
 
     var die = function(msg) {
         process.stderr.write('Error: ' + msg + '\n');
+        process.stderr.write(USAGE + '\n');
         process.exit(1);
     };
 
